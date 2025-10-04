177a9fea6c578d6207ea48671447ccf983700473
mallou <malloudev@gmail.com>
2025-10-05 01:32:42 +0200

    Changed ingress rules to per vps ones and added pm2

--- execute.sh
+++ execute.sh
@@ -11,7 +11,7 @@ sudo apt update
 sudo apt install -y cloudflared
 
 # Download typescript and retranspile the code just in case
-yes | sudo npm i -g typescript 
+yes | sudo npm i -g typescript pm2
 bash ./buildscript.sh
 
 # Setting up the cloudflare tunnel
@@ -26,24 +26,36 @@ done
 cloudflared login --token "$token"
 uuid=$(basename ~/.cloudflared/*.json .json)
 
+for argument in "$@"; do
+   case $argument in vps=*)
+      vps="${argument#vps=}"
+      break;;
+   esac
+done
+
 cat > "$HOME/.cloudflared/config.yml" << EOL
 tunnel: $uuid
 credentials-file: $HOME/.cloudflared/$uuid.json
 
 ingress:
-  - hostname: db1.aetheris.mallou.dev
-    service: tcp://127.0.0.1:5000
-  - hostname: db2.aetheris.mallou.dev
-    service: tcp://127.0.0.1:6000
+  - hostname: db$vps.aetheris.mallou.dev
+    service: tcp://127.0.0.1:5432
+  - hostname: vps$vps.aetheris.mallou.dev
+    service: ssh://127.0.0.1:22
   - hostname: aetheris.mallou.dev
-    service: http://127.0.0.1:7000
-  - hostname: api.aetheris.mallou.dev
     service: http://127.0.0.1:8000
+  - hostname: api.aetheris.mallou.dev
+    service: http://127.0.0.1:9000
   - service: http_status:404
 EOL
 
 sudo cloudflared service install
 sudo systemctl enable cloudflared
+
+# Configure PM2 to always run the javascript code
+sudo pm2 start ./dist/commonjs/index.js --name main
+sudo pm2 save
+sudo pm2 startup systemd -u $USER --hp $HOME
 
 # Reboot to make all services reboot
 sudo reboot
